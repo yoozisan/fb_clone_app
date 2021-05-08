7ee506948e687fefac2fb0e3e805ae0cc977dfb9
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-08 18:21:03 +0900

    CRUD機能作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_020808) do
+ActiveRecord::Schema.define(version: 2021_05_08_062457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_05_07_020808) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "pictures", "users"
 end
