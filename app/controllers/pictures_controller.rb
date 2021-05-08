0ca3c8843862bbde3e0c3456b66ebd249a755faa
yoichiro kawai <acchonburike.416@gmail.com>
2021-05-09 00:44:00 +0900

    アップローダー実装中

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -49,7 +49,7 @@ class PicturesController < ApplicationController
 
   private
   def picture_params
-    params.require(:picture).permit(:title, :content)
+    params.require(:picture).permit(:title, :content, :image, :image_cache)
   end
 
   def set_picture
